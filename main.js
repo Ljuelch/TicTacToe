f8950df6a16b5333be36e7552515b067323836e4
Ljuelch <79401648+Ljuelch@users.noreply.github.com>
2021-11-26 18:58:11 +0100

    Revert "fix das"

--- main.js
+++ main.js
@@ -13,28 +13,9 @@ const winConditions = [
     [2, 4, 6]
 ];
 
-const winIDs = [
-    [document.getElementById("field_0"), document.getElementById("field_1"), document.getElementById("field_2")],
-    [document.getElementById("field_1"), document.getElementById("field_4"), document.getElementById("field_7")],
-    [document.getElementById("field_6"), document.getElementById("field_7"), document.getElementById("field_8")],
-    [document.getElementById("field_0"), document.getElementById("field_3"), document.getElementById("field_6")],
-    [document.getElementById("field_1"), document.getElementById("field_4"), document.getElementById("field_7")],
-    [document.getElementById("field_2"), document.getElementById("field_5"), document.getElementById("field_8")],
-    [document.getElementById("field_0"), document.getElementById("field_4"), document.getElementById("field_8")],
-    [document.getElementById("field_2"), document.getElementById("field_4"), document.getElementById("field_6")]
-];
-
-// console.log(winIDs);
-// let länge = document.getElementById("field_0");
-// console.log(länge.innerText.length);
-
-
 //document.addEventListener("DOMContentLoaded", main());
 main();
-filterIdName();
-// textLength();
-
-// checkWin();
+checkWin();
 
 function main() {
     let cells = document.getElementsByClassName("cell");
@@ -64,105 +45,15 @@ function isValidInput(cell) {
     return cell.innerText === "";
 }
 
+function checkWin() {
 
-function filterIdName() {
-    var name = document.getElementsByClassName('cell');
-    for (let i = 0; i < name.length; i++) {
-        name[i].onclick = function() {
-            var idAusgabe = this.id;
-            this.id;
-            console.log(idAusgabe);
-        }
-    };
+    winConditions.forEach(win => {
+        console.log("check next Win");
+        win.forEach( id => {
+            console.log("field_" + id);
+            console.log(document.getElementById("field_5"));
+        })
+
+    });
 }
-
-filterIdName();
-
-
-
-
-// alle id winConditions in mehrdimensionales array packen 
-
-// nach jedem click  durchlaufen und schauen ob alle strings gefüllt sind (mit.length?)
-    // wenn nicht alle strings gefüllt sind dann weiter
-    // wenn alle strings gefüllt 
-        //mit else if checken ob alle den selben text haben 
-            // wenn nicht selber text dann weiter 
-            // wenn selber text dann Game win !!!!!
-
-            // const winIDss = [
-            //     [document.getElementById("field_0"), document.getElementById("field_1"), document.getElementById("field_2")],
-            //     [document.getElementById("field_1"), document.getElementById("field_4"), document.getElementById("field_7")],
-            //     [document.getElementById("field_6"), document.getElementById("field_7"), document.getElementById("field_8")],
-            //     [document.getElementById("field_0"), document.getElementById("field_3"), document.getElementById("field_6")],
-            //     [document.getElementById("field_1"), document.getElementById("field_4"), document.getElementById("field_7")],
-            //     [document.getElementById("field_2"), document.getElementById("field_5"), document.getElementById("field_8")],
-            //     [document.getElementById("field_0"), document.getElementById("field_4"), document.getElementById("field_8")],
-            //     [document.getElementById("field_2"), document.getElementById("field_4"), document.getElementById("field_6")]
-            // ];
-
-
-// function textLength() {
-//     for (let i = 0; i < winIDs.length; i++) {
-//         winIDs[i].onClick = function() {
-//             // let länge = document.getElementById("field_0");
-//             if (winIDs.innerHTML === "X") {
-//                 console.log("bin da");
-//             }
-//         }
-//     };
-// }
-// textLength();
-
-
-// function textLength() {
-//     for (let i = 0; i < winIDs.length; i++) {
-//         winIDs.forEach(kontrolle => {
-            
-//         });
-//     };
-// }
-// textLength();
-
-
-
-
-
-
-
-
-//let länge = document.getElementById("field_0");
-//console.log(länge.innerHTML.length);
-
-
-// function checkWin() {
-//     winConditions.forEach(win => {
-//         console.log("---");
-//         win.forEach( id => {
-//             let idAusgabe = "field_" + id;
-//             console.log(idAusgabe);
-//         })
-//     });
-// }
-
-// function checkWin() {
-//     winConditions.forEach(win => {
-//         console.log("---");
-//         win.forEach( id => {
-//            // console.log("field_" + id);
-//             if (("field_" + [0] == filterIdName())&& ("field_" + [1] == filterIdName()) && ("field_" + [2] == filterIdName())) {
-//                 console.log("geil");
-//             }else  {
-//                // console.log("weiter");
-//             }
-//         })
-//     });
-// }
-
-
-
-// class spielfeld element anklicken dann klasse ausgeben 
-// durch winConditions loopen und jedes mal "feld_" +arraystelle  mit id vergleichen
-// wenn a == b && b == c 
-// dann return win 
 
