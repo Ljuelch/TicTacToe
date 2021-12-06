5f7e353bc8fea1e516f8fddb05dcc7e4c498fd4d
Ljuelch <leojuelch@yahoo.de>
2021-12-06 23:34:00 +0100

    Updates/cleanup/fixes with Till

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const playerX = "X";
 const playerO = "O";
-var nextPlayer = playerX;
+var currentPlayer = playerX;
 var activeGame = document.getElementById("spielfeld");
+
 activeGame.disable = false;
+
+const status = {
+    nothing: "",
+    win: "win",
+    tie: "tie"
+}
 
 const winConditions = [
     [0, 1, 2],
@@ -16,80 +23,123 @@ const winConditions = [
     [2, 4, 6]
 ];
 
+function restart() {
+    document.getElementById("restartGame").addEventListener("click", function () {
+        location.reload();
+    })
+}
+
+function reset() {
+    document.getElementById("resetGame").addEventListener("click", function () {
+        localStorage.clear();
+        location.reload();
+    })
+}
+
 function main() {
+    document.getElementById("ties").innerText = localStorage.getItem("ties");
+    document.getElementById("winsPlayerX").innerText = localStorage.getItem("winsPlayerX");
+    document.getElementById("winsPlayerO").innerText = localStorage.getItem("winsPlayerO");
     let cells = document.getElementsByClassName("cell");
     for (const cell of cells) {
         cell.addEventListener("click", function () {
-            if (activeGame.disable) {
+            if (activeGame.disable || !isValidInput(this)) {
                 return;
             }
-            if (isValidInput(this)) {
-                printField(this);
-                checkWin();
-                changePlayer();
-            }
+            printField(this);
+            checkWin();
+            changePlayer();
         });
     }
-}
-main();
-
-function printField(cell) {
-    cell.innerText = nextPlayer;
-}
-
-function changeH2(nextPlayer) {
-    let uebersicht = document.getElementById("changePlayer");
-    uebersicht.innerHTML = (`Spieler ${nextPlayer} ist am Zug`);
-}
-
-function changePlayer() {
-    if (nextPlayer === playerX) {
-        nextPlayer = playerO;
-    } else {
-        nextPlayer = playerX;
-    }
-    changeH2(nextPlayer);
 }
 
 function isValidInput(cell) {
     return cell.innerText === "";
 }
 
+function printField(cell) {
+    cell.innerText = currentPlayer;
+}
+
+function changePlayer() {
+    currentPlayer = playerX === currentPlayer ? playerO : playerX;
+    announcePlayer(currentPlayer);
+    localStorage.setItem("player", currentPlayer);
+}
+
+function announcePlayer(nextPlayer) {
+    document.getElementById("messageBox").innerHTML = (`Spieler ${nextPlayer} ist am Zug`);
+}
+
 function checkWin() {
-    const players = [playerX, playerO];
-    let   fields  = [];
-
-    for (let i = 0; i < 9; i++) {
-        const field = document.getElementById("field_" + i);
-        fields[i] = (field.innerText || "").trim() + field.id.split("_")[1];
-    }
-
-    for (const player of players) {
-        for (const condition of winConditions) {
-            for (const i in condition) {
-                if (fields.indexOf(player + condition[i]) == -1) {
-                    break;
-                }
-                if (i == condition.length -1) {
-                    activeGame.disable = true;
-                    setTimeout(winMessage, 10, player);
-                }
-            }
+    let ties = 0;
+    for (const condition of winConditions) {
+        let solution = hasWinner(condition);
+        if (solution === status.win) {
+            activeGame.disable = true;
+            incrementWinningPlayer(solution);
+            let message = `Spieler ${currentPlayer} hat gewonnen!`;
+            setTimeout(endGameMessage, 10, message);
+            return;
         }
+        if (solution === status.tie) {
+            ties++;
+
+        }
+
+    }
+    if (ties === winConditions.length) {
+        let countTies = localStorage.getItem("ties");
+        localStorage.setItem("ties", ++countTies + "");
+        document.getElementById("ties").innerText = countTies;
+        activeGame.disable = true;
+        let message = "Unentschieden!";
+        setTimeout(endGameMessage, 10, message);
     }
 }
 
-function winMessage(player) {
-    document.getElementById("wm").setAttribute("id", "popUp");
-    document.getElementById("pu").setAttribute("id", "winMessage");
-    document.getElementById("winMessage").innerText = `Spieler ${player} hat gewonnen!`;
+function incrementWinningPlayer() {
+    let player = "winsPlayer" + currentPlayer;
+    let countWinningPlayer = localStorage.getItem(player);
+    localStorage.setItem(player, ++countWinningPlayer + "");
+    document.getElementById(player).innerText = countWinningPlayer;
 }
 
-function restart() {
-    let btn = document.getElementById("restart-game");
-    btn.addEventListener("click", function () {
-        window.location.reload();
-    })
+function hasWinner(condition) {
+    let previousValue = "";
+    let state = status.win;
+    for (const targetFieldIndex in condition) {
+        let value = document.getElementById("field_" + condition[targetFieldIndex]).innerText;
+
+        if (value === "") {
+            state = status.nothing;
+            continue;
+        }
+
+        if (previousValue !== "" && previousValue !== value) {
+            return status.tie;
+        }
+
+        previousValue = value;
+    }
+    return state;
 }
+
+function endGameMessage(message) {
+    document.getElementById("messageBox").innerText = message;
+}
+
+function setLocalStorage (item) {
+    if (localStorage.getItem(item) == null) {
+        localStorage.setItem(item, "0");
+    }
+}
+
+setLocalStorage("ties");
+setLocalStorage("winPlayerX");
+setLocalStorage("winPlayerO");
+
+main();
 restart();
+reset();
 
